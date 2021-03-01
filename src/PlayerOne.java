f669fad1ef8fc417352f76b70e2f9384af966c09
Al-Rou <roal.24.alro@gmail.com>
2021-02-28 23:14:17 -0500

    Game underway1

--- src/PlayerOne.java
+++ src/PlayerOne.java
@@ -1,2 +1,22 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Random;
+
 public class PlayerOne {
+    public static void main(String[] args)
+    {
+        Random random = new Random();
+        try{
+            DatagramSocket datagramSocket = new DatagramSocket(5557);
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            byteBuffer.order(ByteOrder.BIG_ENDIAN);
+            byteBuffer.putInt(random.nextInt(100));
+            DatagramPacket datagramPacket = new DatagramPacket(byteBuffer.array(), byteBuffer.position(),
+                    InetAddress.getLocalHost(), 5558);
+            datagramSocket.send(datagramPacket);
+        }
+    }
 }
