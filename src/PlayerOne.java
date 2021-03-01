c108a748014ac065cab2092b2fd2c89775fa2603
Al-Rou <roal.24.alro@gmail.com>
2021-03-01 01:05:47 -0500

    The code is now commented for a better readability!

--- src/PlayerOne.java
+++ src/PlayerOne.java
@@ -9,43 +9,51 @@ import java.util.Scanner;
 public class PlayerOne {
     public static void main(String[] args)
     {
+        //Player One can choose if he/she starts by themselves or lets the computer go
         System.out.println("Which one would you like to choose?(Type the number)");
         System.out.println("1) I enter my own number");
         System.out.println("2) Computer shall go automatically");
         Scanner in = new Scanner(System.in);
         int choiceNumber = in.nextInt();
-        int sendingNumber = 0;
+        //Sending number has to be declared here, until it can be initialized either by the user or by the machine
+        int sendingNumber;
         if(choiceNumber == 1)
         {
-            System.out.println("Enter a whole number between 1 to 100");
+            System.out.println("Enter a whole number between 2 to 100");
             sendingNumber = in.nextInt();
         }
         else {
             Random random = new Random();
             sendingNumber = random.nextInt(100);
         }
+        //Now, a connection is established and the first number is sent to Player Two
         try{
             DatagramSocket datagramSocket = new DatagramSocket(5557);
+            //This loop continues sending and receiving data until the game finishes
             do {
+                //Sending data
                 ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
                 byteBuffer.order(ByteOrder.BIG_ENDIAN);
                 byteBuffer.putInt(sendingNumber);
                 DatagramPacket datagramPacket = new DatagramPacket(byteBuffer.array(), byteBuffer.position(),
                         InetAddress.getLocalHost(), 5558);
                 datagramSocket.send(datagramPacket);
-
+                //Receiving data
                 ByteBuffer byteBufferResponse = ByteBuffer.allocate(1024);
                 byteBufferResponse.order(ByteOrder.BIG_ENDIAN);
                 DatagramPacket datagramPacketRes = new DatagramPacket(byteBufferResponse.array(),
                         byteBufferResponse.array().length);
                 datagramSocket.receive(datagramPacketRes);
                 int receivedNumber = byteBufferResponse.getInt();
+                //Here, the received number from Player Two is checked to see if he has already won
+                //This checking is important in order to finish the connection in case Player Two has won the game
                 if (receivedNumber == 0)
                 {
                     System.out.println("Player One lost the game!");
                     return;
                 }
                 else {
+                    //Processing the received number according to rules of the game
                     if ((receivedNumber % 3) == 0) {
                         System.out.println(receivedNumber + " + 0 = " + receivedNumber);
                         sendingNumber = receivedNumber / 3;
@@ -64,7 +72,9 @@ public class PlayerOne {
                     }
                 }
             }while (sendingNumber > 1);
+            //Leaving the loop means that Player One has won the game
             System.out.println("End of the game: Player One won!");
+            //So, number zero is sent to Player Two to signal him that he should stop waiting for new numbers
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
             byteBuffer.order(ByteOrder.BIG_ENDIAN);
             byteBuffer.putInt(0);
--- src/PlayerTwo.java
+++ src/PlayerTwo.java
@@ -6,22 +6,28 @@ import java.nio.ByteOrder;
 public class PlayerTwo {
     public static void main(String[] args)
     {
+        //A connection is established and the first number is received from Player One
         try {
             DatagramSocket datagramSocket = new DatagramSocket(5558);
+            //This loop continues sending and receiving data until the game finishes
             while (true){
+                //Receiving data
                 ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024);
                 byteBuffer2.order(ByteOrder.BIG_ENDIAN);
                 DatagramPacket datagramPacket2 = new DatagramPacket(byteBuffer2.array(),
                         byteBuffer2.array().length);
                 datagramSocket.receive(datagramPacket2);
                 int receivedNumber = byteBuffer2.getInt();
+                //Here, the received number from Player One is checked to see if he has already won
+                //This checking is important in order to finish the connection in case Player One has won the game
                 if(receivedNumber == 0)
                 {
                     System.out.println("Player Two lost the game!");
                     break;
                 }
                 else {
-                    int sendingNumber = 0;
+                    int sendingNumber;
+                    //Processing the received number according to rules of the game
                     if ((receivedNumber % 3) == 0) {
                         System.out.println(receivedNumber + " + 0 = " + receivedNumber);
                         sendingNumber = receivedNumber / 3;
@@ -38,8 +44,10 @@ public class PlayerTwo {
                         System.out.println("Sending number is " + ((receivedNumber + 1) / 3));
                         System.out.println("************");
                     }
+                    //Reaching 1 means that Player Two has won the game
                     if (sendingNumber == 1) {
                         System.out.println("End of the game: Player Two won!");
+                        //So, number zero is sent to Player One to signal him that he should stop waiting for new numbers
                         ByteBuffer byteBuffer3 = ByteBuffer.allocate(1024);
                         byteBuffer3.order(ByteOrder.BIG_ENDIAN);
                         byteBuffer3.putInt(0);
